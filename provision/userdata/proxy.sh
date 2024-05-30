ce109be8fb9209e0b87087eea58865dbfd309da1
lausbelphegor <lausbelphegor@gmail.com>
2024-05-30 09:33:48 +0300

    chore: Install project dependencies in proxy.sh
    This commit updates the proxy.sh script to include the installation of project dependencies using the requirements.txt file. This ensures that the necessary packages are installed before running the main.py script.

--- provision/userdata/proxy.sh
+++ provision/userdata/proxy.sh
@@ -16,4 +16,5 @@ python3 -m venv /opt/471_proxy_project/venv
 sudo git clone https://github.com/lausbelphegor/471_proxy_project.git /opt/471_proxy_project
 sudo python3 -m venv /opt/471_proxy_project/venv
 source /opt/471_proxy_project/venv/bin/activate
+sudo python3 -m pip install -r /opt/471_proxy_project/requirements.txt
 python3 /opt/471_proxy_project/main.py
